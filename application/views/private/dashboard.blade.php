0005f00c1ca4d561dd55f069437af78e1d77b69d
KurangKering <cybercature@gmail.com>
2021-09-25 17:09:38 +0700

    perbaikan

--- application/views/private/dashboard.blade.php
+++ application/views/private/dashboard.blade.php
@@ -2,31 +2,31 @@
 @section('css')
 @endsection
 @section('content')
-<main class="main">
-	<!-- Breadcrumb-->
-	<ol class="breadcrumb"></ol>
-	<div class="container-fluid">
-		<div class="animated fadeIn">
-			<!-- /.row-->
-			<div class="row">
-				<div class="col-md-12">
-					<div class="card" style="min-height: 71vh;">
+    <main class="main">
+        <!-- Breadcrumb-->
+        <ol class="breadcrumb"></ol>
+        <div class="container-fluid">
+            <div class="animated fadeIn">
+                <!-- /.row-->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card" style="min-height: 71vh;">
 
-						<div class="card-body">
-							<div class="text-center">
-							{{-- <h2><strong>SELAMAT DATANG DI</strong> <br> --}}
-								{{-- Sistem Peminjaman dan Inventory Sarana Prasarana. --}}
-							</h2>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- /.col-->
-			</div>
-			<!-- /.row-->
-		</div>
-	</div>
-</main>
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <h2><strong>SELAMAT DATANG DI</strong> <br>
+                                        Sistem Peminjaman dan Inventory Sarana Prasarana.
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.col-->
+                </div>
+                <!-- /.row-->
+            </div>
+        </div>
+    </main>
 @endsection
 @section('js')
 @endsection
